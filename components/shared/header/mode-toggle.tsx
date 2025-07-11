678a1c6ddd21e9de5dea306eb9858da45dbb9706
Eniola Awopegba <whoiseniola@Eniolas-MacBook-Air.local>
2025-07-11 10:11:09 +0100

    Complete the Basic Layout

---
+++ components/shared/header/mode-toggle.tsx
@@ -0,0 +1,49 @@
+'use client';
+import { useTheme } from "next-themes";
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuContent, DropdownMenuCheckboxItem } from "../../ui/dropdown-menu";
+import { SunIcon, MoonIcon, SunMoon } from 'lucide-react';
+import { Button } from "../../ui/button";
+import {useState, useEffect} from 'react';
+
+const ModeToggle = () => {
+    const [mounted, setMounted] = useState(false);
+    const { theme, setTheme } = useTheme();
+
+    useEffect(() => {
+        setMounted(true);
+    });
+
+    if(!mounted){
+        return null;
+    }
+
+    return ( <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+            <Button variant = 'ghost' className='focus-visible:ring-0 focus-visible:ring-offset-0'>
+                { theme === 'system' ? (
+                    <SunMoon />
+                ) : theme === 'dark' ? (
+                    <MoonIcon />
+                ) : (
+                    <SunIcon />
+                )
+            }
+            </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+            <DropdownMenuLabel>Appearance</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuCheckboxItem checked={theme === 'system'} onClick={() => setTheme('system')}>
+                System
+            </DropdownMenuCheckboxItem>
+            <DropdownMenuCheckboxItem checked={theme === 'dark'} onClick={() => setTheme('dark')}>
+                Dark
+            </DropdownMenuCheckboxItem>
+            <DropdownMenuCheckboxItem checked={theme === 'light'} onClick={() => setTheme('light')}>
+                Light
+            </DropdownMenuCheckboxItem>
+        </DropdownMenuContent>
+    </DropdownMenu> );
+}
+ 
+export default ModeToggle;
